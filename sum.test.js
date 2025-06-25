923ff15f839259bd7ae0b3db69cd8555e51e898d
gato4d <alvaro.asalgado@ug.uchile.cl>
2025-06-24 21:36:04 -0400

    prueba de ci

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,7 @@
 const sum = require('./sum'); // Import the sum function from sum.js
 
 test('Suma dos numeros', () => {
-    expect(sum(1, 2)).toBe(3); // toBe es un matcher de Jest que verifica que el resultado sea igual a 3
+    expect(sum(1, 2, 3)).toBe(6); // toBe es un matcher de Jest que verifica que el resultado sea igual a 6
 });
 
 // test unitario de la funcion sum que verificar que la suma de 1 y 2 es igual a 3
