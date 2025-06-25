81c76f781a1d34179df07c2c44528e06c2a1cd53
gato4d <alvaro.asalgado@ug.uchile.cl>
2025-06-24 21:22:41 -0400

    se crea proyecto en node.js para testear gh-actions

---
+++ sum.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+    return a + b;
+}
+
+module.exports = sum; // Exporta la funcion sum para que pueda ser utilizada en otros archivos
+// Pueder ser importada en otros archivos utilizando require('./sum')... VER ARCHIVO sum.test.js
---
+++ sum.test.js
@@ -0,0 +1,8 @@
+const sum = require('./sum'); // Import the sum function from sum.js
+
+test('Suma dos numeros', () => {
+    expect(sum(1, 2)).toBe(3); // toBe es un matcher de Jest que verifica que el resultado sea igual a 3
+});
+
+// test unitario de la funcion sum que verificar que la suma de 1 y 2 es igual a 3
+// utiliza el framework de pruebas Jest
